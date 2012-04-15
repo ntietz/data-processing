a170122051ad3e7bd63c4a6785c1cafeaf88bbe9
Nicole Tietz-Sokolskaya <me@ntietz.com>
2012-04-14 21:04:18 -0400

    Began work on implementation of mapper/reducer

--- src/com/treecitysoftware/algorithm/pagerank/PageRankMapper.java
+++ src/com/treecitysoftware/algorithm/pagerank/PageRankMapper.java
@@ -1,9 +1,25 @@
 package com.treecitysoftware.algorithm.pagerank;
 
+import com.treecitysoftware.data.*;
+
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+
+import java.io.*;
+
 public class PageRankMapper
 extends MapReduceBase
-implements Mapper<Key, Value, Key, Value>
+implements Mapper<Text, Node, Text, Writable>
 {
+    public void map( Text key
+                   , Node value
+                   , OutputCollector<Text, Writable> output
+                   , Reporter reporter
+                   )
+    throws IOException
+    {
+        // TODO add mapper content
+    }
 
 }
 
--- src/com/treecitysoftware/algorithm/pagerank/PageRankReducer.java
+++ src/com/treecitysoftware/algorithm/pagerank/PageRankReducer.java
@@ -1,9 +1,25 @@
 package com.treecitysoftware.algorithm.pagerank;
 
+import com.treecitysoftware.data.*;
+
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+
+import java.io.*;
+import java.util.*;
+
 public class PageRankReducer
 extends MapReduceBase
-implements Reducer<Key, Value, Key, Value>
+implements Reducer<Text, Writable, Text, Node>
 {
-
+    public void reduce( Text key
+                      , Iterator<Writable> values
+                      , OutputCollector<Text, Node> output
+                      , Reporter reporter
+                      )
+    throws IOException
+    {
+        // TODO add reducer content
+    }
 }
 
