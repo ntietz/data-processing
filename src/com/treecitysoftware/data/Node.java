0caf4a34d7f2c5f73a29a104001d8c4442bb48cb
Nicholas Tietz <ntietz@gmail.com>
2012-04-15 21:55:17 -0400

    Added javadoc comments to Node.

--- src/com/treecitysoftware/data/Node.java
+++ src/com/treecitysoftware/data/Node.java
@@ -4,10 +4,36 @@ import java.util.*;
 
 public class Node<T>
 {
+    /**
+     * id is used within the system to identify nodes in lists of links and for supplementary information.
+     */
     private String id;
+
+    /**
+     * value is a generic type so the payload of the node can be fitted to the problem.
+     * In PageRank, we will use value type Double so we can track the PageRank scores.
+     * In a normal graph, we could have PageRank be the contents of the node, such as a WikiPage.
+     */
     private T value;
+
+    /**
+     * neighbors stores the ids of all the adjacent nodes.
+     * Each of these ids should exist as the id field of another node.
+     */
     private List<String> neighbors;
 
+    /**
+     * There is no safe or desired default behavior, so the default constructor is disabled.
+     */
+    private Node()
+    {
+        // no safe or desired default behavior
+    }
+
+    /**
+     * @param i id for the new node
+     * @param v value of the new node
+     */
     public Node(String i, T v)
     {
         id = i;
@@ -15,6 +41,11 @@ public class Node<T>
         neighbors = new ArrayList<String>();
     }
 
+    /**
+     * @param i id for the new node
+     * @param v value of the new node
+     * @param n list of neighbor ids of the new node
+     */
     public Node(String i, T v, List<String> n)
     {
         id = i;
@@ -22,26 +53,44 @@ public class Node<T>
         neighbors = n;
     }
 
+    /**
+     * @return the id of the node
+     */
     public String getId()
     {
         return id;
     }
 
+    /**
+     * @return the value of the node
+     */
     public T getValue()
     {
         return value;
     }
 
+    /**
+     * @return the neighbors of the node
+     */
     public List<String> getNeighbors()
     {
         return neighbors;
     }
 
+    /**
+     * Adds the id of a neighbor to the list.
+     * Does not verify whether the id is already in the list.
+     * @param i id of the neighbor to add.
+     */
     public void addNeighbor(String i)
     {
         neighbors.add(i);
     }
 
+    /**
+     * Removes all instances of the neighbor from the neighbor list.
+     * @param i id of the neighbor to remove
+     */
     public void removeNeighbor(String i)
     {
         boolean removing = true;
@@ -51,11 +100,21 @@ public class Node<T>
         }
     }
 
+    /**
+     * Checks whether or not the id is a neighbor of the current node.
+     * @param i id of the possible neighbor
+     * @return  true if the id is a neighbor, false otherwise
+     */
     public boolean connectsTo(String i)
     {
         return neighbors.contains(i);
     }
 
+    /**
+     * Checks whether or not the node is a dangling node.
+     * Dangling nodes are those which have no neighbors.
+     * @return  true if the node is dangling, false otherwise
+     */
     public boolean isDangling()
     {
         if (neighbors.size() == 0)
@@ -67,5 +126,11 @@ public class Node<T>
             return false;
         }
     }
+
+    // TODO add equals
+
+    // TODO add compareTo
+
+    // TODO add hashCode
 }
 
