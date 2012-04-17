19f3141307a59b2b20583de47acfe8403a23f7e1
Nicole Tietz-Sokolskaya <me@ntietz.com>
2012-04-16 23:58:31 -0400

    Finished pagerank reducer.

--- src/com/treecitysoftware/algorithm/pagerank/PageRankReducer.java
+++ src/com/treecitysoftware/algorithm/pagerank/PageRankReducer.java
@@ -13,10 +13,12 @@ extends MapReduceBase
 implements Reducer<Text, Writable, Text, Node>
 {
     private long numberOfNodes;
+    private double dampingFactor;
 
     public  void configure(JobConf conf)
     {
         numberOfNodes = Long.valueOf(conf.get("numberOfNodes"));
+        dampingFactor = Double.valueOf(conf.get("dampingFactor"));
     }
 
     public void reduce( Text key
@@ -26,7 +28,29 @@ implements Reducer<Text, Writable, Text, Node>
                       )
     throws IOException
     {
-        // TODO add reducer content
+        Node node = new Node(key.toString(), new DoubleWritable(0.0));
+        double score = 0.0;
+
+        while (values.hasNext())
+        {
+            Writable obj = values.next();
+
+            if (obj instanceof Node)
+            {
+                node = (Node) obj;
+            }
+            else if (obj instanceof Contribution)
+            {
+                Contribution contribution = (Contribution) obj;
+                score += contribution.getAmount();
+            }
+        }
+
+        score = dampingFactor / numberOfNodes + (1 - dampingFactor) * score;
+
+        node.setValue(new DoubleWritable(score));
+
+        output.collect(key, node);
     }
 }
 
