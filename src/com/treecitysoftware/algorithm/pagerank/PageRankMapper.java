cd9434ce5c4279b95d0a3c3ac3bf1b22aba25543
Nicole Tietz-Sokolskaya <me@ntietz.com>
2012-04-17 00:09:04 -0400

    Added javadoc comments to PageRank mapper and reducer.

--- src/com/treecitysoftware/algorithm/pagerank/PageRankMapper.java
+++ src/com/treecitysoftware/algorithm/pagerank/PageRankMapper.java
@@ -12,6 +12,15 @@ public class PageRankMapper
 extends MapReduceBase
 implements Mapper<Text, Node, Text, Writable>
 {
+    /**
+     * Takes in (id, node) pairs and outputs one (id, node) and many (id, contribution) pairs.
+     * The outgoing id in the (id, node) pair is the same as the incoming.
+     * The outgoing id in the (id, contribution) pair is for which neighbor the contribution goes to.
+     * @param key       the id of the incoming pair
+     * @param value     the node of the incoming pair
+     * @param output    collects the output pairs for the reducer
+     * @param reporter  allows sending counts back to the job driver
+     */
     public void map( Text key
                    , Node value
                    , OutputCollector<Text, Writable> output
