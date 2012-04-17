9ec59ac6fb01fa358ef21c5a8129e6d2d822dcc0
Nicholas Tietz <ntietz@gmail.com>
2012-04-17 00:09:04 -0400

    Added javadoc comments to PageRank mapper and reducer.

--- src/com/treecitysoftware/algorithm/pagerank/PageRankReducer.java
+++ src/com/treecitysoftware/algorithm/pagerank/PageRankReducer.java
@@ -12,15 +12,37 @@ public class PageRankReducer
 extends MapReduceBase
 implements Reducer<Text, Writable, Text, Node>
 {
+    /**
+     * The number of nodes in the entire graph.
+     */
     private long numberOfNodes;
+
+    /**
+     * The damping factor helps the PageRank algorithm converge.
+     * It is often set between 0.15 and 0.30.
+     */
     private double dampingFactor;
 
+    /**
+     * Initializes the numberOfNodes and dampingFactor, transmitted from the job driver.
+     * This is run before reduce is run.
+     * @param conf  allows reading values set by the job driver
+     */
     public  void configure(JobConf conf)
     {
         numberOfNodes = Long.valueOf(conf.get("numberOfNodes"));
         dampingFactor = Double.valueOf(conf.get("dampingFactor"));
     }
 
+    /**
+     * Takes in an id and list of values (node or contributions) and outputs a single id-node pair.
+     * The input from the mapper should only have one node be received.
+     * Exactly one node should always be received.
+     * @param key       the id of the node receiving contributions
+     * @param values    a list of either Node (once) or Contribution (many times)
+     * @param output    collects the output pairs
+     * @param reporter  allows sending counts back to the job driver
+     */
     public void reduce( Text key
                       , Iterator<Writable> values
                       , OutputCollector<Text, Node> output
