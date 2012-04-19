68abc34aa9bcbd43e7455582f56a1830043cb3f5
Nicholas Tietz <ntietz@gmail.com>
2012-04-19 18:28:00 -0400

    Finished graph parser preliminaries.

--- src/com/treecitysoftware/tool/wikipedia/GraphParser.java
+++ src/com/treecitysoftware/tool/wikipedia/GraphParser.java
@@ -2,24 +2,34 @@ package com.treecitysoftware.tool.wikipedia;
 
 import com.treecitysoftware.data.*;
 
+import java.io.*;
 import java.util.*;
 
 public class GraphParser
 {
     public static void main(String... args)
+    throws FileNotFoundException // TODO handle this in the arg handling
+         , IOException
     {
         // args[0] : page path
         // args[1] : edge path
         // args[2] : output graph path
         // args[3] : output log path
+        String inputPagePath = args[0];
+        String inputEdgePath = args[1];
+        String outputGraphPath = args[2];
+        String outputLogPath = args[3];
 
-        // open page path
-        List<String> lines = null;
+        BufferedReader pageIn = new BufferedReader(new InputStreamReader(new DataInputStream(new FileInputStream(inputPagePath))));
+        BufferedReader edgeIn = new BufferedReader(new InputStreamReader(new DataInputStream(new FileInputStream(inputEdgePath))));
+        PrintStream graphOut = new PrintStream(new FileOutputStream(outputGraphPath));
+        PrintStream logOut = new PrintStream(new FileOutputStream(outputLogPath));
 
         // read in all the pages, store pageTitle -> id in a map
         Map<String, String> pageToIdMap = new HashMap<String, String>();
 
-        for (String each : lines)
+        String each = pageIn.readLine();
+        while (each != null)
         {
             DataLine line = new DataLine(each);
 
@@ -30,18 +40,19 @@ public class GraphParser
                 for (StringPair page : pages)
                 {
                     pageToIdMap.put(page.right, page.left);
+                    graphOut.println(page.right + "\t" + page.left);
                 }
             }
             else
             {
                 // output to the log TODO
             }
+
+            each = pageIn.readLine();
         }
 
-        // read in all the edges, output to file as we go: [edge (id -> id)]
-        lines = null;
-
-        for (String each : lines)
+        each = edgeIn.readLine();
+        while (each != null)
         {
             DataLine line = new DataLine(each);
 
@@ -55,12 +66,18 @@ public class GraphParser
 
                     // if the id exists, output the page
                     // otherwise log it!
+
+                    // TODO BAD NICHOLAS BAD !!!!
+                    logOut.println(edge.left + "\t" + edge.right);
+                    // TODO CHANGE THIS TODO ONLY TEMPORARY
                 }
             }
             else
             {
                 // output to the log TODO
             }
+
+            each = edgeIn.readLine();
         }
 
     }
