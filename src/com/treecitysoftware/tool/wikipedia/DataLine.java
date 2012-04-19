98b04b3b784fffa2a96d5f8b33500ef05a167c8c
Nicole Tietz-Sokolskaya <me@ntietz.com>
2012-04-19 18:28:00 -0400

    Finished graph parser preliminaries.

--- src/com/treecitysoftware/tool/wikipedia/DataLine.java
+++ src/com/treecitysoftware/tool/wikipedia/DataLine.java
@@ -63,6 +63,8 @@ public class DataLine
 
         do
         {
+            //System.out.println(pages.size() + "\t" + strippedLine.length());
+
             int firstComma = strippedLine.indexOf(',');
             int secondComma = strippedLine.indexOf(',', firstComma + 1);
             int endingQuote = strippedLine.indexOf('\'', secondComma + 2);
@@ -71,10 +73,12 @@ public class DataLine
                 endingQuote = strippedLine.indexOf('\'', endingQuote + 1);
             }
 
-            int endIndex = endingQuote + 1;
+            int endIndex = strippedLine.indexOf(')', endingQuote + 1) + 1;
 
             String thisPage = strippedLine.substring(0, endIndex);
             strippedLine = strippedLine.substring(endIndex+1, strippedLine.length());
+
+            //System.out.println(thisPage);
 
             String id = thisPage.substring(1, firstComma);
             String namespace = thisPage.substring(firstComma + 1, secondComma);
@@ -87,17 +91,59 @@ public class DataLine
                 pages.add(page);
             }
 
-        } while (!(strippedLine.charAt(0) == ';'));
+        } while (strippedLine.length() > 1);
 
         return pages;
     }
+
 
     /**
      * @return  a list of stringpairs where left is the originating page id, right is the target page title
      */
     public List<StringPair> getEdges()
     {
-        return null;
+        if (!containsEdgeData())
+        {
+            return null;
+        }
+
+        List<StringPair> pages = new ArrayList<StringPair>();
+
+        String strippedLine = line.substring(edgePrefix.length(), line.length()); // TODO might have to change this index +- 1
+
+        do
+        {
+            //System.out.println(pages.size() + "\t" + strippedLine.length());
+
+            int firstComma = strippedLine.indexOf(',');
+            int secondComma = strippedLine.indexOf(',', firstComma + 1);
+            int endingQuote = strippedLine.indexOf('\'', secondComma + 2);
+            while (strippedLine.charAt(endingQuote - 1) == '\\')
+            {
+                endingQuote = strippedLine.indexOf('\'', endingQuote + 1);
+            }
+
+            int endIndex = endingQuote + 2;
+
+            String thisPage = strippedLine.substring(0, endIndex);
+            strippedLine = strippedLine.substring(endIndex+1, strippedLine.length());
+
+            //System.out.println(thisPage);
+
+            String id = thisPage.substring(1, firstComma);
+            String namespace = thisPage.substring(firstComma + 1, secondComma);
+
+            if (namespace.equals("0"))
+            {
+                String title = thisPage.substring(secondComma + 2, endingQuote);
+
+                StringPair page = new StringPair(id, title);
+                pages.add(page);
+            }
+
+        } while (strippedLine.length() > 1);
+
+        return pages;
     }
 
 }
