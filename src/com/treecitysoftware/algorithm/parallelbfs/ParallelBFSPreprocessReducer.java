7833729743730a50183e57afe035d637689fe00c
James Baiera <jbaiera@kent.edu>
2012-04-22 21:55:06 -0400

    Added the functionality to the Preprocessor mapper and reducer

---
+++ src/com/treecitysoftware/algorithm/parallelbfs/ParallelBFSPreprocessReducer.java
@@ -0,0 +1,113 @@
+package com.treecitysoftware.algorithm.parallelbfs;
+
+import com.treecitysoftware.data.*;
+
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.*;
+
+import java.io.*;
+import java.util.*;
+
+public class ParallelBFSPreprocessReducer
+extends MapReduceBase
+implements Reducer<IntWritable, Node, IntWritable, Node>
+{
+    /**
+     * The ID of the sourceNode that we are currently
+     * searching from.
+     */
+    private int sourceNodeID;
+
+    /**
+     * Flag for first run set at true to start, 
+     * set at false first time reduce is run
+     */
+    private boolean first = true;
+
+    /**
+     * Name of the file that stores the list of target node IDs
+     */
+    private String targetNodeFileName;
+
+    /**
+     * Set that is created to hold the nodes in the target file
+     */
+    private Set<int> targetNodes;
+
+    /**
+     * Runs before any reducing begins; Sets the file name and 
+     * the sourceNodeID's
+     * @param conf The JobConf object that is created by the driver
+     */
+    public void configure(JobConf conf)
+    {
+        sourceNodeID = Integer.valueOf(conf.get("sourceNodeID"));
+        targetNodeFileName = conf.get("targetNodeFileName");
+        targetNodes = new TreeSet<int>();
+    }
+
+    /**
+     * Recieves (id, node) pairs from the mappers and for each pair
+     * it assigns a payload of type BFSStatus.
+     * If a node is from the target list, it marks it as so.
+     * If the node is the source, it gets the first distance assigned to it.
+     * @param key The node's id
+     * @param value The collection of Node objects that correspond to the ID
+     * @param output An OutputCollector object to recieve the initialized nodes
+     * @param reporter The default reporting object
+     */
+    public void reduce(IntWritable key
+                     , Iterator<Node> value
+                     , OutputCollector<IntWritable, Node> output
+                     , Reporter reporter
+                     )
+    throws IOException
+    {
+        //First time?
+        if(first)
+        {
+            //Load in all of the target nodes into the tree set
+            first = false;
+
+            FileSystem fs = FileSystem.get(new Configuration());
+            BufferedReader in = new BufferedReader(
+                                    new InputStreamReader(
+                                        fs.open(
+                                            new Path(
+                                                targetNodeFileName
+                                ))));
+
+            String line = in.readLine();
+
+            while (line != null)
+            {
+                targetNodes.add(Integer.valueOf(line));
+            }
+        }
+      
+        //Hand it a blank BFSStatus (infinity, no path, not target)
+        Node node = value.next();
+        node.setValue(new BFSStatus());
+
+        //if target nodes contains the current ID:
+        if(targetNodes.contains(key.get()))
+        {
+            //Set it as a target node
+            node.getValue().setTarget(true);
+        }
+
+        //if the node is the source
+        if(key.get() == sourceNodeID)
+        {
+            //set the path to itself, and set the distance to 0.
+            node.getValue().setDistance(0);
+            node.getValue().getPath().add(key.get());
+        }
+
+        //Emit the nodeID and Node
+        output.collect(key, node);
+    }
+}
