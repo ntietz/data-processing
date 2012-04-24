5a95c15825a09abfd7827431a46f142957e72886
Nicole Tietz-Sokolskaya <me@ntietz.com>
2012-04-23 21:38:57 -0400

    Removed stupid files.

--- src/com/treecitysoftware/algorithm/pagerank/ConvergenceReducer.java
+++
@@ -1,9 +0,0 @@
-package com.treecitysoftware.algorithm.pagerank;
-
-public class ConvergenceReducer
-//extends MapReduceBase
-//implements Reducer<Text, DoubleWritable, Text, DoubleWritable>
-{
-
-}
-
