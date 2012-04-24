fc62dbabeedd5e11671104a80d00dfe0af7889b9
Nicholas Tietz <ntietz@gmail.com>
2012-04-23 21:38:29 -0400

    Added PageRankPreprocessor mapper and reducer.

---
+++ src/com/treecitysoftware/algorithm/pagerank/PageRankPreprocessMapper.java
@@ -0,0 +1,25 @@
+package com.treecitysoftware.algorithm.pagerank;
+
+import com.treecitysoftware.data.*;
+
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+
+import java.io.*;
+
+public class PageRankPreprocessMapper
+extends MapReduceBase
+implements Mapper<IntWritable, WikiPage, IntWritable, WikiPage>
+{
+    public void map( IntWritable key
+                   , WikiPage value
+                   , OutputCollector<IntWritable, WikiPage> output
+                   , Reporter reporter
+                   )
+    throws IOException
+    {
+        output.collect(key, value);
+    }
+
+}
+
