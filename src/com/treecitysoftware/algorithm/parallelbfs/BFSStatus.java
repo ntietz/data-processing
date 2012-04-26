b59e683ff0e902d47571ac7f12cc1c26351c2e86
James Baiera <jbaiera@kent.edu>
2012-04-26 01:13:16 -0400

    Added expansion flag and expansion set to BFSStatus.java

--- src/com/treecitysoftware/algorithm/parallelbfs/BFSStatus.java
+++ src/com/treecitysoftware/algorithm/parallelbfs/BFSStatus.java
@@ -15,20 +15,36 @@ implements Writable
     private int distance;
 
     /**
-     * Default Constructor returns a BFSStatus with an empty path and 
-     * distance at logical infinity and not a target
+     * Set true if the node needs to expand
+     */
+    private boolean toExpand;
+
+    /**
+     * Contains list of node ID's to send distance to
+     */
+    private Set<Integer> whoTo;
+
+    /**
+     * Default Constructor returns a BFSStatus with distance at 
+     * logical infinity, not to expand, and an empty Set of integers
      */
     public BFSStatus()
     {
         distance = Integer.MAX_VALUE;
+        toExpand = false;
+        whoTo = new TreeSet<Integer>();
     }
 
     /**
      * @param startingDistance The distance from the starting node
+     * @param expanding Whether or not the node should expand
+     * @param nodesToSendData The set of neighbors that we have not heard from
      */
-    public BFSStatus(int startingDistance)
+    public BFSStatus(int startingDistance, boolean expanding, Set<Integer> nodesToSendData)
     {
         distance = startingDistance;
+        toExpand = expanding;
+        whoTo = nodesToSendData;
     }
 
     /**
@@ -49,6 +65,41 @@ implements Writable
         distance = newDistance;
     }
 
+
+    /**
+     * @return whether or not the node needs expansion
+     */
+    public boolean needsExpansion()
+    {
+        return toExpand;
+    }
+
+    /**
+     * @param needToExpand true if we want to make it expand
+     */
+    public void setExpansion(boolean needToExpand)
+    {
+        toExpand = needToExpand;
+    }
+
+    /**
+     * @return The set of nodes we need to send data to if the
+     * page needs to expand.
+     */
+    public Set<Integer> whoToSendTo()
+    {
+        return whoTo;
+    }
+
+    /**
+     * @param nodesToSendTo The nodes we want to send data to when
+     * we expand.
+     */
+    public void setWhoTo(Set<Integer> nodesToSendTo)
+    {
+        whoTo = nodesToSendTo;
+    }
+
     /**
      * Deserializes the object out to the disk
      * @param out DataOutput object used to serialize the data to disk
@@ -57,6 +108,13 @@ implements Writable
     throws IOException
     {
         out.writeInt(distance);
+        out.writeBoolean(toExpand);
+        int cardinality = whoTo.size();
+        out.writeInt(cardinality);
+        for (Integer eachID : whoTo)
+        {
+            out.writeInt(eachID.intValue());
+        }
     }
 
     /**
@@ -67,5 +125,11 @@ implements Writable
     throws IOException
     {
         distance = in.readInt();
+        toExpand = in.readBoolean();
+        int cardinality = in.readInt();
+        for (int idx = 0; idx < cardinality; idx++)
+        {
+            whoTo.add(new Integer(in.readInt()));
+        }
     }
 }
