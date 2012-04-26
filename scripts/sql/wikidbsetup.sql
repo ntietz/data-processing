b66e50f3f48797ee2853035e49a93e7498286d21
Jason Buffin <jason@loverofmysoul.com>
2012-04-25 20:12:01 -0400

    added SQL DDL file usage: login to mysql and run: source file.sql   it will add the tables if they dont exist

--- scripts/sql/wikidbsetup.sql
+++ scripts/sql/wikidbsetup.sql
@@ -2,9 +2,9 @@
 -- set up the tables for the graph structure on MySQL
 -- 
 
-DROP TABLE IF EXISTS `pages`;
+-- DROP TABLE IF EXISTS `pages`;
 
-CREATE TABLE 
+CREATE TABLE IF NOT EXISTS
     `pages`
     ( `page_id` int(8) unsigned NOT NULL DEFAULT '0'
     , `title` varbinary(255) NOT NULL DEFAULT ''
@@ -12,23 +12,21 @@ CREATE TABLE
     , PRIMARY KEY (`page_id`)
     ) ENGINE=InnoDB;
     
-DROP TABLE IF EXISTS `links`;
+-- DROP TABLE IF EXISTS `links`;
     
-CREATE TABLE
+CREATE TABLE IF NOT EXISTS
     `links`
     ( `from_id` int(8) unsigned NOT NULL DEFAULT '0'
     , `to_id` int(8) unsigned NOT NULL DEFAULT '0'
     , PRIMARY KEY (`from_id`, `to_id`)
     ) ENGINE=InnoDB;
 
-DROP TABLE IF EXISTS `bfs`;
+-- DROP TABLE IF EXISTS `bfs`;
 
-CREATE TABLE 
+CREATE TABLE IF NOT EXISTS
     `bfs`
     ( `from_id` int (8) unsigned NOT NULL DEFAULT '0'
     , `to_id` int(8) unsigned NOT NULL DEFAULT '0'
     , `hops` tinyint(1) unsigned NOT NULL DEFAULT '0'
     , PRIMARY KEY (`from_id`, `to_id`)
     ) ENGINE=InnoDB;
-    
-    
