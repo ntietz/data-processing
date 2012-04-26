187a480f053764ad8ba358cb44376a63a2fe1a7b
James Baiera <jbaiera@kent.edu>
2012-04-26 02:04:21 -0400

    Added the BFSChange object to the library

---
+++ src/com/treecitysoftware/algorithm/parallelbfs/BFSChange.java
@@ -0,0 +1,94 @@
+package com.treecitysoftware.algorithm.parallelbfs;
+
+import com.treecitysoftware.algorithm.parallelbfs.*;
+import com.treecitysoftware.data.*;
+
+import org.apache.hadoop.io.*;
+
+import java.io.*;
+import java.util.*;
+
+public class BFSChange
+extends Writable
+{
+    /**
+     * Holds the page node id that the change came from
+     */
+    private int from;
+
+    /**
+     * Holds the distance to transmit
+     */
+    private int distance
+
+    /**
+     * Constructs an empty change object
+     */
+    public BFSChange()
+    {
+        // No expected behavior
+    }
+
+    /**
+     * Constructs a BFSChange object
+     * @param id ID of the node that sent the change
+     * @param d Distance that the node will send
+     */
+    public BFSChange(int id, int d)
+    {
+        from = id;
+        distance = d;
+    }
+
+    /**
+     * @return the node id that sent this change
+     */
+    public int getSendingNode()
+    {
+        return from;
+    }
+
+    /**
+     * @param id The ID of the node that is sending this change
+     */
+    public void setSendingNode(int id)
+    {
+        from = id;
+    }
+
+    /**
+     * @return the distance of the change
+     */
+    public int getDistance()
+    {
+        return distance;
+    }
+
+    /**
+     * @param d The distance that the node is sending
+     */
+    public void setDistance(int d)
+    {
+        distance = d;
+    }
+    
+    /**
+     * Serializes the change to disk
+     * @param out DataOutput that serializes it out
+     */
+    public void write(DataOutput out)
+    {
+        out.writeInt(from);
+        out.writeInt(distance);
+    }
+    
+    /**
+     * Serializes the change from disk
+     * @param in DataInput that serializes it in
+     */
+    public void readFields(DataInput in)
+    {
+        from = in.readInt();
+        distance = in.readInt();
+    }
+}
