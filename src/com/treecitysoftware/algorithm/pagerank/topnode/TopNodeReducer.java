13eb442830cc68bbfeeef74cba58920d8e3c7597
Nicholas Tietz <ntietz@gmail.com>
2012-04-25 20:20:29 -0400

    Added the imports... build succeeds now.

--- src/com/treecitysoftware/algorithm/pagerank/topnode/TopNodeReducer.java
+++ src/com/treecitysoftware/algorithm/pagerank/topnode/TopNodeReducer.java
@@ -1,5 +1,13 @@
 package com.treecitysoftware.algorithm.pagerank.topnode;
 
+import com.treecitysoftware.data.*;
+
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+
+import java.io.*;
+import java.util.*;
+
 public class TopNodeReducer
 extends MapReduceBase
 implements Reducer<DoubleWritable, IntWritable, IntWritable, DoubleWritable>
@@ -8,7 +16,7 @@ implements Reducer<DoubleWritable, IntWritable, IntWritable, DoubleWritable>
     private int numberOutputed = 0;
 
     public void reduce( DoubleWritable key
-                      , Iterator<IntWritable> value
+                      , Iterator<IntWritable> values
                       , OutputCollector<IntWritable, DoubleWritable> output
                       , Reporter reporter
                       )
